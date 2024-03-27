652cc9027fa7cacec9b24e02050e052b23320aa2
Shivansh <155874081+shivansh-mangla@users.noreply.github.com>
2024-03-27 09:38:01 +0000

    Added Bootstrap and basic template render

--- app.py
+++ app.py
@@ -1,10 +1,34 @@
-from flask import Flask , render_template , request
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+JOBS = [{
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Bengaluru, India',
+    'salary': 100
+}, {
+    'id': 2,
+    'title': 'Data Scientist',
+    'location': 'Delhi, India',
+    'saalary': 200
+}, {
+    'id': 3,
+    'title': 'Frontend Engineer',
+    'location': 'Remote',
+    'salary': 300
+}, {
+    'id': 4,
+    'title': 'Backend Engineer',
+    'location': 'San Francisco, USA',
+    'salary': 400
+}]
+
+
 @app.route("/")
 def hello_world():
-    return render_template('home.html')
+  return render_template('home.html', jobs=JOBS , company_name = 'Piku')
+
 
 if __name__ == "__main__":
-  app.run(host = '0.0.0.0', debug = True)
+  app.run(host='0.0.0.0', debug=True)
