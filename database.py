1c01900d5715b68cd6580934625e7cb55968da4e
Shivansh <155874081+shivansh-mangla@users.noreply.github.com>
2024-03-28 02:26:25 +0000

    stored application in database

--- database.py
+++ database.py
@@ -19,3 +19,22 @@ def load_job_from_db(id):
   if len(data) == 0:
     return None
   return data[0]
+
+
+def add_application_to_db(job_id, data):
+  supabase.table('applications').insert([{
+      'job_id':
+      job_id,
+      'full_name':
+      data['full_name'],
+      'email':
+      data['email'],
+      'linkedin_url':
+      data['linkedin_url'],
+      'education':
+      data['education'],
+      'work_experience':
+      data['work_experience'],
+      'resume_url':
+      data['resume_url']
+  }]).execute()
